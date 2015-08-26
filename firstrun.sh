b7d91b6e44f24fa37ba563e0c5cc825004ec3408
aptalca <thedjinn@yahoo.com>
2015-08-26 13:23:03 -0400

    Create firstrun.sh

---
+++ firstrun.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Get docker env timezone and set system timezone
+echo "setting the correct local time"
+echo $TZ > /etc/timezone
+export DEBCONF_NONINTERACTIVE_SEEN=true DEBIAN_FRONTEND=noninteractive
+dpkg-reconfigure tzdata
+cd /config
+
+if [ ! -f /config/ha-bridge-"$VERSION".jar ]; then
+  echo "Installing version '$VERSION'"
+  wget http://www.bwssystems.com/files/ha-bridge-"$VERSION".jar
+else
+  echo "Using existing version '$VERSION'"
+fi
+echo "Setting correct permissions"
+chown -R nobody:users /config
+
+#check to see if SERVERPORT variable is set, if not, set it to default
+if [ -z "$SERVERPORT" ]; then
+  echo "SERVERPORT environment not passed, using default port 8080"
+  SERVERPORT=8080
+fi
+
+/sbin/setuser nobody java -Xmx312M -jar -Djava.net.preferIPv4Stack=true amazon-echo-bridge-"$VERSION".jar -Dupnp.config.address=$SERVERIP -Dserver.port=$SERVERPORT -Dvera.address=$VERAIP
