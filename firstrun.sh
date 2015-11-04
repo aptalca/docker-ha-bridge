1a4decf2c0a6978e5c6cf63952f2fc5c73a28d79
aptalca <thedjinn@yahoo.com>
2015-11-04 13:15:33 -0500

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -22,4 +22,22 @@ if [ -z "$SERVERPORT" ]; then
   SERVERPORT=8080
 fi
 
-/sbin/setuser nobody java -jar -Djava.net.preferIPv4Stack=true -Dupnp.config.address=$SERVERIP -Dserver.port=$SERVERPORT -Dvera.address=$VERAIP -Dupnp.strict=true -Dvtwo.compatibility=false ha-bridge-"$VERSION".jar 2>&1 | tee /config/ha-bridge.log
+ADDPARAM="-Dupnp.config.address=$SERVERIP -Dserver.port=$SERVERPORT -Dupnp.strict=$UPNPSTRICT -Dtrace.upnp=$UPNPTRACE"
+echo -e "Parameters used:\nServer IP : $SERVERIP\nServer Port : $SERVERPORT\nUpnp.Strict : $UPNPSTRICT\nTrace.upnp : $UPNPTRACE"
+
+if [ ! $VERAIP="None" ]; then
+  ADDPARAM+=" -Dvera.address=$VERAIP"
+  echo "Vera connected at IP address : $VERAIP"
+else
+  echo "Vera not connected"
+fi
+
+if [ ! $HARMONYIP="None" ]; then
+  ADDPARAM+=" -Dharmony.address=$HARMONYIP Dharmony.user=$HARMONYUSER -Dharmony.pwd=$HARMONYPWD"
+  echo "Harmony connected at IP address : $HARMONYIP"
+else
+  echo "Harmony not connected"
+fi
+
+echo "Starting Home Automation Bridge"
+/sbin/setuser nobody java -jar "$ADDPARAM" ha-bridge-"$VERSION".jar 2>&1 | tee /config/ha-bridge.log
