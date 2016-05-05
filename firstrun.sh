f3cbd67fbd54986f48d337615f695ffe540239cb
aptalca <thedjinn@yahoo.com>
2016-05-05 00:11:51 -0400

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -6,11 +6,15 @@ echo $TZ > /etc/timezone
 export DEBCONF_NONINTERACTIVE_SEEN=true DEBIAN_FRONTEND=noninteractive
 dpkg-reconfigure tzdata
 cd /config
+if [ ! -z $VERSION ]; then
+  echo "Manual version override:" $VERSION
+else
+  #Check the latest version on github
+  VERSION="$(curl -sX GET https://api.github.com/repos/bwssytems/ha-bridge/releases/latest | grep 'tag_name' | cut -d\" -f4)"
+  VERSION=${VERSION:1}
+  echo "Latest version on bwssystems github repo is" $VERSION
+fi
 
-#Check the latest version on github
-VERSION="$(curl -sX GET https://api.github.com/repos/bwssytems/ha-bridge/releases/latest | grep 'tag_name' | cut -d\" -f4)"
-VERSION=${VERSION:1}
-echo "Latest version on bwssystems github repo is" $VERSION
 if [ ! -f /config/ha-bridge-"$VERSION".jar ]; then
   echo "Installing version '$VERSION'"
   wget https://github.com/bwssytems/ha-bridge/releases/download/v"$VERSION"/ha-bridge-"$VERSION".jar
